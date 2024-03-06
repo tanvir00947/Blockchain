a027de487f973f843b5b1791b610820caf844b2c
tanvir00947 <80541552+tanvir00947@users.noreply.github.com>
2024-03-06 18:20:20 +0000

    first commit

--- Lottery.sol
+++ Lottery.sol
@@ -1,1 +1,42 @@
-x
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    receive() payable external {
+        require(msg.value == 0.001 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns (uint) {
+        require(msg.sender == manager, "You are not the manager");
+        return address(this).balance;
+    }
+
+    function random() internal view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+        require(players.length >= 3);
+
+        uint r = random();
+        address payable winner;
+
+        uint index = r % players.length;
+        winner = players[index];
+
+        winner.transfer(address(this).balance);
+
+        players = new address payable[](0);
+    }
+}
+
